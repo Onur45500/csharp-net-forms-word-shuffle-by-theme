77f0d6631d40193af4ba05de68352b737f6d3342
Onur45500 <onur.akmese@viacesi.fr>
2024-10-05 23:27:27 +0200

    Made the cars theme

--- csharp-net-forms-word-shuffle-by-theme/Cars.Designer.cs
+++ csharp-net-forms-word-shuffle-by-theme/Cars.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             Title = new Label();
+            textBox1 = new TextBox();
+            lblWord = new Label();
+            lblCounter = new Label();
+            lblGuessed = new Label();
             SuspendLayout();
             // 
             // Title
@@ -41,14 +45,58 @@
             Title.TabIndex = 2;
             Title.Text = "Cars";
             // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(291, 180);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(215, 43);
+            textBox1.TabIndex = 3;
+            // 
+            // lblWord
+            // 
+            lblWord.AutoSize = true;
+            lblWord.Font = new Font("Segoe UI", 12F);
+            lblWord.Location = new Point(365, 117);
+            lblWord.Name = "lblWord";
+            lblWord.Size = new Size(65, 28);
+            lblWord.TabIndex = 4;
+            lblWord.Text = "label1";
+            // 
+            // lblCounter
+            // 
+            lblCounter.AutoSize = true;
+            lblCounter.CausesValidation = false;
+            lblCounter.Font = new Font("Segoe UI", 12F);
+            lblCounter.Location = new Point(365, 270);
+            lblCounter.Name = "lblCounter";
+            lblCounter.Size = new Size(65, 28);
+            lblCounter.TabIndex = 5;
+            lblCounter.Text = "label1";
+            // 
+            // lblGuessed
+            // 
+            lblGuessed.AutoSize = true;
+            lblGuessed.Font = new Font("Segoe UI", 12F);
+            lblGuessed.Location = new Point(545, 190);
+            lblGuessed.Name = "lblGuessed";
+            lblGuessed.Size = new Size(65, 28);
+            lblGuessed.TabIndex = 6;
+            lblGuessed.Text = "label1";
+            // 
             // Cars
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblGuessed);
+            Controls.Add(lblCounter);
+            Controls.Add(lblWord);
+            Controls.Add(textBox1);
             Controls.Add(Title);
             Name = "Cars";
             Text = "Cars";
+            KeyPress += KeyIsPressed;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -56,5 +104,9 @@
         #endregion
 
         private Label Title;
+        private TextBox textBox1;
+        private Label lblWord;
+        private Label lblCounter;
+        private Label lblGuessed;
     }
 }
