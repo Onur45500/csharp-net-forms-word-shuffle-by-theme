a663bf6a52290545c1dd780660e098a187e8e8a6
Onur45500 <onur.akmese@viacesi.fr>
2024-10-05 21:36:18 +0200

    Initialized the project

---
+++ csharp-net-forms-word-shuffle-by-theme/Form1.cs
@@ -0,0 +1,10 @@
+namespace csharp_net_forms_word_shuffle_by_theme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
